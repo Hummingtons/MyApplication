9ac0e0c33cf030d028c170742bfa11cf515dcbb8
Jose Ma. A. Pulmones Jr <21jmpulmones@gmail.com>
2023-02-11 18:12:14 +0800

    Initial MyApplication

---
+++ app/src/main/java/com/example/practicecodinghere/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.practicecodinghere;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        String tag = "Main Activity";
+
+        Log.v(tag, "This is a verbose log");
+        Log.d(tag, "This is a debug log");
+        Log.i(tag, "This is an info log");
+        Log.w(tag, "This is a warn log");
+        Log.e(tag, "This is an error log");
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(tag, "This is a debug log");
+            }
+        });
+    }
+}
